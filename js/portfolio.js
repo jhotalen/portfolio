3b62e55266ae38ee66b7ad1e8a52d23688fc4ae0
Jeremy Hotalen <hotalenjeremy@gmail.com>
2020-10-27 16:14:36 -0400

    Week 5 Day 2

---
+++ js/portfolio.js
@@ -0,0 +1,8 @@
+function hideAboutText() {
+    var x = document.getElementById("aboutTextBox");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+  } else {
+        x.style.display = "none";
+    }
+  }
