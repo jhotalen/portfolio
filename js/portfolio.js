2f873364d3eec0765bf17af3b5dbbfea3337ed6b
Jeremy Hotalen <hotalenjeremy@gmail.com>
2020-10-30 22:01:42 -0400

    Week 5 Friday 1st

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,30 +1,33 @@
 function hideAboutText() {
-    var x = document.getElementById("aboutTextBox");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+  var x = document.getElementById("aboutTextBox");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-        x.style.display = "none";
-    }
+    x.style.display = "none";
   }
+}
 
 function hideMenuText() {
-    var x = document.getElementById("menuTextBox");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+  var x = document.getElementById("menuTextBox");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-        x.style.display = "none";
-    }
+    x.style.display = "none";
   }
+}
 
-  function hideShopCarousel() {
-    var x = document.getElementById("carouselContainer");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+function hideShopCarousel() {
+  var x = document.getElementById("carouselContainer");
+  if (x.style.display === "none") {
+    x.style.display = "block";
   } else {
-        x.style.display = "none";
-    }
+    x.style.display = "none";
   }
+}
 
-$("#eggButton").click(function(){ 
-    $("#eggModal").modal("toggle")  
-  });
+$("#eggButton").click(function () {
+  $("#eggModal").modal("toggle")
+});
+
+
+
